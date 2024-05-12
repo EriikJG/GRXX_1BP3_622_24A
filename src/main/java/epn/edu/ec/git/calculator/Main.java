a3275fc86a0c55de29c94758bb7401f920aead68
EriikJG <94009110+EriikJG@users.noreply.github.com>
2024-05-12 05:10:42 -0500

    refactor Main - new Target

--- src/main/java/epn/edu/ec/git/calculator/Main.java
+++ src/main/java/epn/edu/ec/git/calculator/Main.java
@@ -1,39 +1,26 @@
 package epn.edu.ec.git.calculator;
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
         // Crear una instancia de Calculator
         Calculator calculator = new Calculator();
 
-        // Crear un Scanner para leer la entrada del usuario
-        Scanner scanner = new Scanner(System.in);
-
-        // Pedir al usuario que ingrese el primer número
-        System.out.print("Ingresa el primer número: ");
-        int num1 = scanner.nextInt();
-
-        // Pedir al usuario que ingrese el segundo número
-        System.out.print("Ingresa el segundo número: ");
-        int num2 = scanner.nextInt();
-
         // Prueba de la suma
-        int sum = calculator.addition(num1, num2);
+        int sum = calculator.addition(5, 3);
         System.out.println("Suma: " + sum);
 
         // Prueba de la resta
-        int difference = calculator.subtraction(num1, num2);
+        int difference = calculator.subtraction(8, 4);
         System.out.println("Resta: " + difference);
-
-        // Prueba de la multiplicación
-        double multiplicationResult = calculator.multiplication(num1, num2);
-        System.out.println("Multiplicación: " + multiplicationResult);
+        // Prueba de la multiplication
+        double multiplicationResult = calculator.multiplication(10, 2);
+        System.out.println("Multiplicacion: " + multiplicationResult);
 
         // Prueba de la división
-        double divisionResult = calculator.division(num1, num2);
+        double divisionResult = calculator.division(10, 2);
         System.out.println("División: " + divisionResult);
 
-        // Cerrar el scanner para liberar recursos
-        scanner.close();
     }
 }
+
